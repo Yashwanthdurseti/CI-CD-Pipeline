57c5d93f4d6698cb618d5dab2d47e9cebe7b1001
Yashwanth Durseti <yashwanthdurseti@gmail.com>
2024-11-02 13:13:21 +0530

    Add CI/CD pipeline with tests

---
+++ tests/app.test.js
@@ -0,0 +1,14 @@
+const request = require('supertest');
+const app = require('../server'); // Ensure the path to `server.js` is correct
+
+describe('GET Endpoints', () => {
+  it('should load the home page', async () => {
+    const res = await request(app).get('/');
+    expect(res.statusCode).toEqual(200);
+  });
+
+  it('should load the contact page', async () => {
+    const res = await request(app).get('/contact');
+    expect(res.statusCode).toEqual(200);
+  });
+});
